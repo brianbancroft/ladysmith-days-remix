744c8ca3eedb0c7ae6e69d85ae1abb2518734dcb
brianbancroft <hello@brianbancroft.ca>
2024-04-03 19:28:28 -0700

    Add a lot of assets

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -17,22 +17,21 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <>
-      <main className="w-full">
-        <section className="" id="hero">
-          <div
-            id="introduction"
-            className="
+    <main className="w-full">
+      <section className="" id="hero">
+        <div
+          id="introduction"
+          className="
               bg-header-homepage
               flex h-screen
               flex-col
               justify-center bg-gray-500 bg-cover
               bg-no-repeat
             "
-            style={{ backgroundPosition: '0 25%' }}
-          >
-            <div
-              className="
+          style={{ backgroundPosition: '0 25%' }}
+        >
+          <div
+            className="
             flex h-full
             flex-col
             items-center bg-blue-700
@@ -43,77 +42,76 @@ export default function Index() {
             text-white
             md:align-baseline
           "
-            >
-              <img
-                src={logo}
-                className="my-12"
-                alt="ladysmith days logo"
-                height={300}
-                width={350}
-              />
-              <h2 className="font-heading fade-in relative right-4 my-12 justify-center rounded-r-xl bg-white p-2 pl-8 text-5xl font-black text-blue-600 md:right-0 md:block md:bg-transparent md:p-0 md:pl-2 md:text-white">
-                Celebrate Ladysmith
-              </h2>
-              <TimerClock />
-              <h3 className="font-heading fade-in-delay my-4 mt-12 text-xl font-light italic opacity-0">
-                august long weekend
-              </h3>
-            </div>
-            <div className="relative ">
-              <ScrollDownIndicator />
-            </div>
+          >
+            <img
+              src={logo}
+              className="my-12"
+              alt="ladysmith days logo"
+              height={300}
+              width={350}
+            />
+            <h2 className="font-heading fade-in relative right-4 my-12 justify-center rounded-r-xl bg-white p-2 pl-8 text-5xl font-black text-blue-600 md:right-0 md:block md:bg-transparent md:p-0 md:pl-2 md:text-white">
+              Celebrate Ladysmith
+            </h2>
+            <TimerClock />
+            <h3 className="font-heading fade-in-delay my-4 mt-12 text-xl font-light italic opacity-0">
+              august long weekend
+            </h3>
           </div>
+          <div className="relative ">
+            <ScrollDownIndicator />
+          </div>
+        </div>
 
-          <SectionEventSummary />
-        </section>
+        <SectionEventSummary />
+      </section>
 
-        <div className="container mx-auto">
-          {/* <SponsorsHomepage /> */}
+      <div className="container mx-auto">
+        {/* <SponsorsHomepage /> */}
 
-          {/* Three variants:x
+        {/* Three variants:x
               1. The plan (early times)
               2. The schedule. (as soon as the schedule is available)
               3. The gratitude (TBD)
              */}
 
-          {/* {scheduleVisible && <SectionSchedule />} */}
-          <SectionOurPlan />
+        {/* {scheduleVisible && <SectionSchedule />} */}
+        <SectionOurPlan />
 
-          {/* <SectionGratitude /> */}
+        {/* <SectionGratitude /> */}
 
-          <SectionSocialContact />
-        </div>
+        <SectionSocialContact />
+      </div>
 
-        <div className="my-2 grid h-52 grid-cols-1 gap-1 md:hidden">
-          <a
-            href="https://ladysmith.ca"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="bg-gray-300 p-2 transition-all duration-500 hover:bg-gray-200"
-          >
-            <div className="bg-sponsor-homepage-1 my-1 flex h-full w-full justify-center bg-contain bg-center bg-no-repeat px-12 py-6"></div>
-          </a>
-
-          <a
-            href="https://www.ladysmithkinsmen.ca/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="bg-gray-300 p-4 transition-all duration-500 hover:bg-gray-200"
-          >
-            <div className="bg-sponsor-homepage-3 my-1  flex h-full w-full justify-center bg-contain bg-center bg-no-repeat px-12 py-6"></div>
-          </a>
-        </div>
-
-        <section
-          id="contact-us"
-          className="flex items-center justify-center bg-cyan-300 px-4 py-7 text-blue-700 shadow-2xl drop-shadow-2xl"
+      <div className="my-2 grid h-52 grid-cols-1 gap-1 md:hidden">
+        <a
+          href="https://ladysmith.ca"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-gray-300 p-2 transition-all duration-500 hover:bg-gray-200"
         >
-          <FormContactGeneral
-            description="If you have any questions about the event, the best ways to get a hold of us are either through this contact form, or through Facebook"
-            handleSubmit={() => {}}
-          />
-        </section>
-      </main>
-    </>
+          <div className="bg-sponsor-homepage-1 my-1 flex h-full w-full justify-center bg-contain bg-center bg-no-repeat px-12 py-6"></div>
+        </a>
+
+        <a
+          href="https://www.ladysmithkinsmen.ca/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-gray-300 p-4 transition-all duration-500 hover:bg-gray-200"
+        >
+          <div className="bg-sponsor-homepage-3 my-1  flex h-full w-full justify-center bg-contain bg-center bg-no-repeat px-12 py-6"></div>
+        </a>
+      </div>
+
+      <section
+        id="contact-us"
+        className="flex items-center justify-center bg-cyan-300 px-4 py-7 text-blue-700 shadow-2xl drop-shadow-2xl"
+      >
+        <FormContactGeneral
+          description="If you have any questions about the event, the best ways to get a hold of us are either through this contact form, or through Facebook"
+          handleSubmit={() => {}}
+        />
+      </section>
+    </main>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,142 @@
 /** @type {import('tailwindcss').Config} */
+function withOpacity(variableName) {
+  return ({ opacityValue }) => {
+    if (opacityValue !== undefined) {
+      return `rgba(var(${variableName}), ${opacityValue})`
+    }
+    return `rgb(var(${variableName}))`
+  }
+}
+
 export default {
   content: ['./app/**/*.{tsx,ts,jsx,js}'],
+  media: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      heading: ['"Baloo Bhai 2"'],
+    },
+    extend: {
+      backgroundImage: {
+        'hero-image': "url('/hero-homepage.jpg')",
+        'hero-citizen-of-the-year': "url('/hero-citizen-of-the-year.jpg')",
+        sponsor: "url('/placeholder-sponsor.jpg')",
+        vendor: "url('/placeholder-vendor.jpg')",
+        volunteer: "url('/placeholder-volunteer.jpg')",
+        placeholder: "url('https://via.placeholder.com/600x320')",
+        fireworks: "url('/fireworks.jpg')",
+        'call-homepage-sponsor':
+          "url('/backgrounds/call-homepage-sponsor.png')",
+        'call-homepage-vendor': "url('/backgrounds/call-homepage-vendor.png')",
+        'call-homepage-volunteer':
+          "url('/backgrounds/call-homepage-volunteer.png')",
+        'call-vendor-fireworks':
+          "url('/backgrounds/call-vendor-fireworks.png')",
+        'call-vendor-general': "url('/backgrounds/link-vendor-merch.png')",
+        'call-vendor-foodvendor': "url('/backgrounds/link-vendor-food.png')",
+        'header-coty': "url('/backgrounds/header-coty.png')",
+        'header-homepage': "url('/backgrounds/header-homepage.png')",
+        'header-sponsor': "url('/backgrounds/header-sponsor.png')",
+        'header-vendor': "url('/backgrounds/header-vendor.png')",
+        'header-volunteer': "url('/backgrounds/header-volunteer.png')",
+        'sponsor-coty-1': "url('/backgrounds/sponsor-coty-1.png')",
+        'sponsor-coty-2': "url('/backgrounds/sponsor-coty-2.png')",
+        'sponsor-coty-3': "url('/backgrounds/sponsor-coty-3.png')",
+        'sponsor-homepage-1': "url('/backgrounds/sponsor-homepage-1.png')",
+        'sponsor-homepage-2': "url('/backgrounds/sponsor-homepage-2.png')",
+        'sponsor-homepage-3': "url('/backgrounds/sponsor-homepage-3.png')",
+        'sponsor-vendor-1': "url('/backgrounds/sponsor-vendor-1.png')",
+        'sponsor-vendor-2': "url('/backgrounds/sponsor-vendor-2.png')",
+        'sponsor-vendor-3': "url('/backgrounds/sponsor-vendor-3.png')",
+        'sponsor-volunteer-1': "url('/backgrounds/sponsor-volunteer-1.png')",
+        'sponsor-volunteer-2': "url('/backgrounds/sponsor-volunteer-2.png')",
+        'sponsor-volunteer-3': "url('/backgrounds/sponsor-volunteer-3.png')",
+        'sponsor-49th': "url('/backgrounds/sponsor-sponsor-49thparralel.png')",
+        'sponsor-711': "url('/backgrounds/sponsor-sponsor-711.png')",
+        'sponsor-411': "url('/backgrounds/sponsor-sponsor-411.png')",
+        'sponsor-annelynndesign':
+          "url('/backgrounds/sponsor-sponsor-annelynndesign.png')",
+        'sponsor-aw': "url('/backgrounds/sponsor-sponsor-aw.png')",
+        'sponsor-bayview': "url('/backgrounds/sponsor-sponsor-bayview.png')",
+        'sponsor-bayview-framing':
+          "url('/backgrounds/sponsor-sponsor-bayview-framing.png')",
+        'sponsor-bethel': "url('/backgrounds/sponsor-sponsor-bethel.png')",
+        'sponsor-bosleys': "url('/backgrounds/sponsor-sponsor-bosleys.png')",
+        'sponsor-chamber':
+          "url('/backgrounds/sponsor-sponsor-chambercommerce.png')",
+        'sponsor-chronicle':
+          "url('/backgrounds/sponsor-sponsor-chronicle.png')",
+        'sponsor-coastaltrucking':
+          "url('/backgrounds/sponsor-sponsor-coastaltrucking.png')",
+        'sponsor-coop': "url('/backgrounds/sponsor-sponsor-coop.png')",
+        'sponsor-countrygrocer':
+          "url('/backgrounds/sponsor-sponsor-countrygrocer.png')",
+        'sponsor-cupe': "url('/backgrounds/sponsor-sponsor-cupe.png')",
+
+        'sponsor-dens': "url('/backgrounds/sponsor-sponsor-dens.png')",
+        'sponsor-dq': "url('/backgrounds/sponsor-sponsor-dairyqueen.png')",
+        'sponsor-dollarstore':
+          "url('/backgrounds/sponsor-sponsor-dollarstore.png')",
+        'sponsor-gfl': "url('/backgrounds/sponsor-sponsor-gfl.png')",
+        'sponsor-eagles': "url('/backgrounds/sponsor-sponsor-eagles.png')",
+        'sponsor-eagles-ladies':
+          "url('/backgrounds/sponsor-sponsor-eagles-ladies.png')",
+        'sponsor-homehardware':
+          "url('/backgrounds/sponsor-sponsor-home-hardware.png')",
+        'sponsor-hillside': "url('/backgrounds/sponsor-sponsor-hillside.png')",
+        'sponsor-ida': "url('/backgrounds/sponsor-sponsor-ida.png')",
+        'sponsor-kinsmen': "url('/backgrounds/sponsor-sponsor-kinsmen.png')",
+        'sponsor-little-valley':
+          "url('/backgrounds/sponsor-sponsor-littlevalley.png')",
+        'sponsor-maritime-society':
+          "url('/backgrounds/sponsor-sponsor-maritime-society.png')",
+        'sponsor-townladysmith':
+          "url('/backgrounds/sponsor-sponsor-ladysmithtown.png')",
+        'sponsor-creditunion':
+          "url('/backgrounds/sponsor-sponsor-ladysmitthcreditunion.png')",
+        'sponsor-legion': "url('/backgrounds/sponsor-sponsor-legion.png')",
+        'sponsor-market2all':
+          "url('/backgrounds/sponsor-sponsor-market2all.png')",
+        'sponsor-mcdonalds':
+          "url('/backgrounds/sponsor-sponsor-mcdonalds.png')",
+        'sponsor-oceanview':
+          "url('/backgrounds/sponsor-sponsor-oceanview.png')",
+        'sponsor-oldtownbakery':
+          "url('/backgrounds/sponsor-sponsor-oldtownbakery.png')",
+        'sponsor-panago': "url('/backgrounds/sponsor-sponsor-panago.png')",
+        'sponsor-pharmasave':
+          "url('/backgrounds/sponsor-sponsor-pharmasave.png')",
+        'sponsor-raider-hansen':
+          "url('/backgrounds/sponsor-sponsor-raiderhansen.png')",
+
+        'sponsor-ryler': "url('/backgrounds/sponsor-sponsor-ryler.png')",
+        'sponsor-saltair-marine':
+          "url('/backgrounds/sponsor-sponsor-saltair-marine.png')",
+        'sponsor-save-on-foods':
+          "url('/backgrounds/sponsor-sponsor-saveonfoods.png')",
+        'sponsor-spyder-graphix':
+          "url('/backgrounds/sponsor-sponsor-spydergraphix.png')",
+        'sponsor-yum-yum': "url('/backgrounds/sponsor-sponsor-yumyum.png')",
+      },
+      backgroundColor: {
+        'ld-light-blue': withOpacity('--ld-light-blue'),
+        'ld-yellow': withOpacity('--ld-yellow'),
+        'ld-dark-blue': withOpacity('--ld-dark-blue'),
+        'ld-red': withOpacity('--ld-red'),
+        'ld-green': withOpacity('--ld-green'),
+        'ld-purple': withOpacity('--ld-purple'),
+      },
+      textColor: {
+        'ld-light-blue': withOpacity('--ld-light-blue'),
+        'ld-yellow': withOpacity('--ld-yellow'),
+        'ld-dark-blue': withOpacity('--ld-dark-blue'),
+        'ld-red': withOpacity('--ld-red'),
+        'ld-green': withOpacity('--ld-green'),
+        'ld-purple': withOpacity('--ld-purple'),
+      },
+    },
+  },
+
+  variants: {
     extend: {},
   },
   plugins: [],
